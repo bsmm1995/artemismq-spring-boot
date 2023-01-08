0f979fdcb079c00475e38fabdae3421b9806a564
Bladimir Minga <bminga@pegasus.cl>
2023-01-08 12:31:16 -0500

    Mejoras

--- msa-consumer/src/main/java/com/java/consumer/ConsumerApplication.java
+++ msa-consumer/src/main/java/com/java/consumer/ConsumerApplication.java
@@ -2,12 +2,12 @@ package com.java.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class ConsumerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
-
 }
